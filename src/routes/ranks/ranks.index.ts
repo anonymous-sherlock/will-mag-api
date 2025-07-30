acc9434bd80ff12c707c7e5fb7eb322c79165695
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2025-07-31 00:26:07 +0530

    feat: setup ranks api

---
+++ src/routes/ranks/ranks.index.ts
@@ -0,0 +1,8 @@
+import { createBaseAPIRouter } from '@/lib/create-app';
+
+import * as handlers from './ranks.handlers';
+import * as routes from './ranks.routes';
+
+const router = createBaseAPIRouter().openapi(routes.list, handlers.list);
+
+export default router;
